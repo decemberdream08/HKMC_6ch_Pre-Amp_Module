dcde696d3df9e5bfe77b3dba0e03a78a46458bea
decemberdream08 <decemberdream08@gmail.com>
2024-11-29 12:01:29 +0900

    2024-11-28 : To fix compile error which is SysTick_Handler_IT.

--- A31G22x_it.c
+++ A31G22x_it.c
@@ -50,9 +50,7 @@ void PendSV_Handler(void)
 
 void SysTick_Handler(void)
 {
-#ifdef DEEP_SLEEP_MODE_ENABLE
-	SysTick_Handler_IT();
-#endif
+
 }
 
 /* SYSTEM */
--- A31G22x_it.h
+++ A31G22x_it.h
@@ -35,9 +35,6 @@ extern void TIMER20_IRQHandler_Interrupt(void);
 #ifdef TIMER21_ENABLE
 extern void TIMER21_IRQHandler_Interrupt(void);
 #endif //TIMER21_ENABLE
-#ifdef DEEP_SLEEP_MODE_ENABLE
-extern void SysTick_Handler_IT(void);
-#endif //DEEP_SLEEP_MODE_ENABLE
 
 #ifdef __cplusplus
 extern "C" {
